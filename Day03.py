b55b0364bdc3d82b20fe0ca8dc7157b2407b9838
tearuka <tearuka@gmail.com>
2022-12-04 10:16:54 +0100

    Implement minor changes

--- Day03.py
+++ Day03.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import string
+
 # read input
 inp = open("inp/input_03.txt").read().splitlines()
 
-# list of letters
-import string
-alphabet_lower = list(string.ascii_lowercase)
-alphabet_upper = list(string.ascii_uppercase)
-letters = alphabet_lower + alphabet_upper
-# list of numbers
+# dictionary of letters and numbers
+alphabet = list(string.ascii_letters)
 numbers = list(range(1,53))
-# create dictionary
-priority_values = dict(zip(letters, numbers))
+priority_values = dict(zip(alphabet, numbers))
 
 # part 1
 sum_one = 0
 for rucksack in inp:
     first_compartment, second_compartment = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
-    common_item = list(set(first_compartment) & set(second_compartment))
-    sum_one += priority_values[common_item[0]]
+    common_item = (set(first_compartment) & set(second_compartment)).pop()
+    sum_one += priority_values[common_item]
 
 print("Result for part 1: ", sum_one)  # 8202
 
@@ -27,9 +24,8 @@ print("Result for part 1: ", sum_one)  # 8202
 # divide input into chunks of three rucksacks
 chunks = [inp[i:i + 3] for i in range(0, len(inp), 3)]
 sum_two = 0
-for group in chunks:
-    first_elf, second_elf, third_elf = group
-    badge = list(set(first_elf) & set(second_elf) & set(third_elf))
-    sum_two += priority_values[badge[0]]
+for first_elf, second_elf, third_elf in chunks:
+    badge = (set(first_elf) & set(second_elf) & set(third_elf)).pop()
+    sum_two += priority_values[badge]
 
 print("Result for part 2: ", sum_two)  # 2864
